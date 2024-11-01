1da63ad9c3618005f9a88ba00882e183789c4c10
Cody <wilcoyonkin@gmail.com>
2024-11-01 16:56:48 -0400

    switched browserRouter to HashRouter to solve safari refreshing problem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -14,22 +14,17 @@ const App = () => {
 
 
   return (
-      <BrowserRouter>
-      
-        <NavigationButtons />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="" element={<Navigate to="/" replace />} />
-
-            <Route path="/about" element={<About />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/projects" element={<Projects />} />
-
-
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-      
-      </BrowserRouter>
+    <Router>
+    <NavigationButtons />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="" element={<Navigate to="/" replace />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/experience" element={<Experience />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </Router>
       
   );
 };
