2721ecf26d36682188d5c2e3da40e313375870eb
Cody <wilcoyonkin@gmail.com>
2024-11-01 12:50:06 -0400

    fixed compiling error

--- src/components/NavigationButtons.js
+++ src/components/NavigationButtons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHref, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const NavigationButtons = () => {
     const navigate = useNavigate();
